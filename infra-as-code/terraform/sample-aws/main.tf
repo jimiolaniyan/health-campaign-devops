687fe613bc95b93c022f7bd9126762e5a8cd7fe9
Olaniyan Folajimi <jimiolaniyan@gmail.com>
2025-08-07 22:07:30 +0100

    Update main.tf

--- infra-as-code/terraform/sample-aws/main.tf
+++ infra-as-code/terraform/sample-aws/main.tf
@@ -13,6 +13,10 @@ terraform {
       source  = "gavinbunney/kubectl"
       version = "~> 1.14.0" 
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0, < 6.0.0"
+    }
   }
 }
 
